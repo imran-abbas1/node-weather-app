b10eb36fa69d3fe8efc7016413f578c63b35b978
Imran Abbas <imran.jafri786@gmail.com>
2020-06-09 11:14:44 +0530

    callback added

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 const request = require('postman-request')
+const geoCode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const url = 'http://api.weatherstack.com/current?access_key=da3dea83e76c2a6f6dcd44d72f7c80af&query=Noida&units=m'
-
-request({url: url, json: true}, (error, response) => {
-    
-    if (error) {
-        console.log('Unable to weather stack service')
-    } else if(response.body.error) {
-        console.log('Unable to find location')
-    } else {
-    const data = (response.body.current)
-    console.log(data.weather_descriptions[0])
-    console.log(`It is currently ${data.temperature} degrees out. It feels like ${data.feelslike}`)
-    }
-    
+const address = process.argv[2]
+if (!address) {
+  return console.log('Please provide an address')
+} else {
+geoCode(address , (error, data) => {
+  
+  if (error) {
+      return console.log('ERROR : ' , error)
+  }
+  
+forecast(data.latitude, data.longitude, (error, forecastData) => {
+  if (error) {
+    return console.log('ERROR : ' , error)
+}
+    console.log(data.location)
+    console.log(forecastData)
 })
-
-const url2= 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaW1yYW5qYWZyaTc4NiIsImEiOiJja2FxbXhmOG8wMWkwMnlxdDJqc3VvZTRiIn0.gCmM-ALmM7vPuTuc0JAkOg&limit=1'
-
-request({url: url2, json: true}, (error, response) => { 
-
-    if (error) {
-        console.log('Unable to connect to mapbox services')
-    } else if(response.body.features.length === 0) {
-        console.log('Unable to fetch location currently')
-    } else {
-    const data = (response)
-    const longi = data.body.features[0].center[0]
-    const lat = data.body.features[0].center[1]
-    console.log(`Latitude: ${lat} Longitude: ${longi}`)
-    }
 })
+}
